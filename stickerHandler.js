1bf7205e4546b1db0680a84cbc31a85e7bd985f8
zard <annurzarqoni@gmail.com>
2026-04-27 03:53:06 +0700

    fix: use center crop instead of padding for video sticker

--- stickerHandler.js
+++ stickerHandler.js
@@ -36,8 +36,7 @@ function convertVideoToWebp(videoBuffer) {
     ffmpeg(inputPath)
       .addOutputOptions([
         '-vcodec', 'libwebp',
-        '-vf', 'scale=512:512:force_original_aspect_ratio=decrease,pad=512:512:(ow-iw)/2:(oh-ih)/2:color=0x00000000,fps=15',
-        '-pix_fmt', 'yuva420p',
+        '-vf', 'scale=512:512:force_original_aspect_ratio=increase,crop=512:512,fps=15',
         '-loop', '0',
         '-ss', '00:00:00',
         '-t', String(MAX_VIDEO_DURATION),
